b0ded3eba9992c51406c5a15aa870761ec0da955
Moses Esan <esanm@rte.ie>
2020-01-03 17:36:20 +0000

    Router added and App.js file updated

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// * Description: App Entry Point
+import React, {Component} from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import Router from './app/router'
+
+export default class App extends Component {
+    render() {
+        return <Router/>;
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/router.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+
+//IMPORT ROUTES
+import AuthStack from "./routes/auth";
+import HomeStack from "./routes/home";
+
+import AuthLoading from "./scenes/auth/AuthLoading";
+import AuthProvider from "./provider";
+
+//APP ROUTES STACK
+const AppStack = createSwitchNavigator(
+    {
+        Loading: AuthLoading,
+        Auth: AuthStack,
+        App: HomeStack
+    },
+    {initialRouteName: 'Loading'}
+);
+
+const Navigator = createAppContainer(AppStack);
+
+export default function Router(props) {
+    return (
+        <AuthProvider>
+            <Navigator/>
+        </AuthProvider>
+    );
+}
