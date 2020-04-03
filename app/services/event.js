46e7f446aef7ada2899749d51da819c583933826
Moses Esan <esanm@rte.ie>
2020-04-03 22:10:36 +0100

    Added Service

---
+++ app/services/event.js
@@ -0,0 +1,85 @@
+import axios from 'axios';
+
+import { API_URL } from '../constants';
+
+//API End Point
+export const EVENT = `${API_URL}/event`;
+export const CATEGORY = `${API_URL}/category`;
+
+//INDEX
+export async function getEvents(params = {}){
+    try{
+        let res = await axios.get(`${EVENT}`, {params});
+
+        return res.data;
+    }catch (e) {
+        throw handler(e)
+    }
+}
+
+//CREATE AND EDIT
+export async function createUpdateEvent(data, id){
+    try{
+        const form_data = new FormData();
+        for ( let key in data )
+            form_data.append(key, data[key]);
+
+
+        const options = {
+            method: !id ? 'POST' : 'PUT',
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "multipart/form-data"
+            },
+            data: form_data,
+            url : id ? `${EVENT}/${id}` : `${EVENT}`
+        };
+
+        let res = await axios(options);
+        return res.data;
+    }catch (e) {
+        throw handler(e);
+    }
+}
+
+//READ
+export async function getEvent(id){
+    try{
+        let res = await axios.get(`${EVENT}/${id}`);
+
+        return res.data;
+    }catch (e) {
+        throw handler(e);
+    }
+}
+
+//DELETE
+export async function deleteEvent(id) {
+    try {
+        let res = await axios.delete(`${EVENT}/${id}`);
+
+        return res.data;
+    } catch (e) {
+        throw handler(e);
+    }
+}
+
+//READ CATEGORIES
+export async function getCategories(params = {}){
+    try{
+        let res = await axios.get(`${CATEGORY}`, {params});
+        return res.data;
+    }catch (e) {
+        throw handler(e)
+    }
+}
+
+export function handler(err) {
+    let error = err;
+
+    if (err.response && err.response.data.hasOwnProperty("message"))
+        error = err.response.data;
+    else if (!err.hasOwnProperty("message")) error = err.toJSON();
+
+    return new Error(error.message);
+}
