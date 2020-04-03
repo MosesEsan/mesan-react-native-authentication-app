3491425299f26cc4d9b55165152a8435626f0641
Moses Esan <esanm@rte.ie>
2020-04-03 21:02:12 +0100

    Added provider

---
+++ app/providers/event.js
@@ -0,0 +1,27 @@
+
+import React, {useMemo, useContext} from 'react';
+
+//IMPORT REDUCER, INITIAL STATE AND ACTION TYPES
+import useActionCreator from "../utils/useActionCreator";
+
+// CONTEXT ===================================
+const EventContext = React.createContext();
+
+function EventProvider(props) {
+    const {state, dispatch, fetch, crud, getStateData} = useActionCreator("events", ["categories"]);
+    const categoryObj = useActionCreator("categories");
+
+    const value = useMemo(() => {
+        return {state, dispatch, fetch, crud, getStateData, categoryObj};
+    }, [state, categoryObj['state']]);
+
+    return (
+        <EventContext.Provider value={value}>
+            {props.children}
+        </EventContext.Provider>
+    );
+}
+
+const useEvent = () => useContext(EventContext);
+export { EventContext, useEvent }
+export default EventProvider;
