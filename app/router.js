943dcf4efee2203638fa285b302031d1bcf9a232
Moses Esan <mosesesan@hotmail.com>
2020-04-26 16:32:18 +0100

    Added Profile Section to Router and Switched to a Tab

--- app/router.js
+++ app/router.js
@@ -1,32 +1,61 @@
 import React from 'react';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 import {createStackNavigator} from 'react-navigation-stack';
+
+import {Icon} from 'react-native-elements';
 
 //IMPORT ROUTES
 import AuthStack from "./routes/auth";
 import AuthLoading from "./scenes/auth/AuthLoading";
 import AuthProvider from "./providers/auth";
 
-import {EventProvider, AddEditEventStack, EventStack} from "./routes/event";
+import {ProfileProvider, ProfileStack} from "./routes/profile";
+import {AddEditEventStack, EventProvider, EventStack} from "./routes/event";
 
-//APP ROUTES STACK
+const TabNavigator = createBottomTabNavigator(
+    {
+        Event: EventStack,
+        Profile: ProfileStack
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            tabBarIcon: ({focused, horizontal, tintColor}) => {
+                const {routeName} = navigation.state;
+                let iconName, iconType = 'ionicon';
+                if (routeName === 'Event') {
+                    iconName = `md-list-box`;
+                } else if (routeName === 'Profile') {
+                    iconName = `cart-plus`;
+                    iconType = `material-community`;
+                }
+
+                return <Icon type={iconType} name={iconName} size={22} color={tintColor}/>
+            }
+        }),
+
+        tabBarOptions: {
+            activeTintColor: "#0a163d",
+            inactiveTintColor: 'gray',
+        },
+    });
+
+
+//MAIN STACK
 const MainStack = createSwitchNavigator(
     {
         Loading: AuthLoading,
         Auth: AuthStack,
-        App: EventStack
+        App: TabNavigator
     },
     {initialRouteName: 'Loading'}
 );
 
+//ROOT STACK
 const RootStack = createStackNavigator(
     {
-        Main: {
-            screen: MainStack,
-        },
-        AddEditEvent: {
-            screen: AddEditEventStack,
-        },
+        Main: MainStack,
+        AddEditEvent: AddEditEventStack,
     },
     {
         mode: 'modal',
@@ -39,7 +68,9 @@ export default function Router(props) {
     return (
         <AuthProvider>
             <EventProvider>
-                <Navigator/>
+                <ProfileProvider>
+                    <Navigator/>
+                </ProfileProvider>
             </EventProvider>
         </AuthProvider>
     );
